82624ba927c890304a8aaf4f369b4404f84d0d87
yaoxw <yxwzjq@163.com>
2026-06-27 13:49:12 +0800

    chore: 升级版本号到 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.webhooksender"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
